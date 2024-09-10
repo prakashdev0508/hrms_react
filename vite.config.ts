6b0a3205b781e50407dc1517bda80690c8640835
Prakash Pandey <prakash.pandey@your-space.in>
2024-09-11 03:51:02 +0530

    resolve production issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'), // This is correct
     },
   },
-})
+});
