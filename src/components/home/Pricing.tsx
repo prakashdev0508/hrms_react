faba2c1435be2c18222ec19cfb5e0f759da99573
Prakash Pandey <prakash.pandey@your-space.in>
2024-10-01 15:06:37 +0530

    vercel check

--- src/components/home/Pricing.tsx
+++ src/components/home/Pricing.tsx
@@ -47,7 +47,7 @@ const Pricing = ({ data }: { data: any }) => {
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
-            Designed for business teams like yours
+            Designed for business teams like yours dkfhi
           </h2>
           <p className="mb-5 font-light text-gray-500 sm:text-xl">
             Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
